23563b5501cbac524aa89acc0729a63f3c04e93a
Naresh Polimera <nareshpolimera@Nareshs-Laptop.local>
2024-10-21 16:40:08 +0530

    Initial UI Desing for Borrowers

--- force-app/main/default/lwc/borrowersComponent/borrowersComponent.js
+++ force-app/main/default/lwc/borrowersComponent/borrowersComponent.js
@@ -1,7 +1,18 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api, track } from 'lwc';
+const CHEVRON_UP = "utility:chevronup";
+const CHEVRON_DOWN = "utility:chevrondown";
 
 export default class BorrowersComponent extends LightningElement {
     @api recordId;
-
+    @track chevronToggle = CHEVRON_UP;
+    chevronHandler(event){
+        console.log(JSON.stringify(event.target.dataset.name));
+        if(event.target.dataset.name === CHEVRON_UP){
+            this.chevronToggle = CHEVRON_DOWN;
+        }
+        else{
+            this.chevronToggle = CHEVRON_UP;
+        }
+    }
     
 }
