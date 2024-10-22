c0e1158af48e9fb33dc3e1e5e4dc9a25798e50f5
Naresh Polimera <nareshpolimera@Nareshs-Laptop.local>
2024-10-22 12:27:33 +0530

    Draft #1

--- force-app/main/default/lwc/borrowersComponent/borrowersComponent.js
+++ force-app/main/default/lwc/borrowersComponent/borrowersComponent.js
@@ -5,13 +5,16 @@ const CHEVRON_DOWN = "utility:chevrondown";
 export default class BorrowersComponent extends LightningElement {
     @api recordId;
     @track chevronToggle = CHEVRON_UP;
+
     chevronHandler(event){
         console.log(JSON.stringify(event.target.dataset.name));
         if(event.target.dataset.name === CHEVRON_UP){
             this.chevronToggle = CHEVRON_DOWN;
+            this.showDetails = false;
         }
         else{
             this.chevronToggle = CHEVRON_UP;
+            this.showDetails = true;
         }
     }
     
--- force-app/main/default/lwc/borrowersComponentContainer/borrowersComponentContainer.js
+++ force-app/main/default/lwc/borrowersComponentContainer/borrowersComponentContainer.js
@@ -1,5 +1,11 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api, track } from 'lwc';
 
 export default class BorrowersComponentContainer extends LightningElement {
     @api recordId;
+
+    connectedCallback(){
+        for (let index = 0; index < 2; index++) {
+            const element = index;
+        }
+    }
 }
